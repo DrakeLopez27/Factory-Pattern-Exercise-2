593eda19795b98380592e200e4dd52274f606272
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-02-20 14:54:04 -0600

    ex2

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             string userInput;
+            bool correctInput;
+            do
+            {
+             correctInput= true;
+
+             Console.WriteLine("What type of database would you like to access?");
+
+             Console.WriteLine("TYPE: SQL, Mongo, List");
+
+             userInput= Console.ReadLine();
+
+                if(userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput= false;
+                    Console.WriteLine("Error: Incorrect Input");
+                }
+                
+
+            }while(correctInput==false);
+            Console.Clear();
+
+             IDataAccess database =  DataAccessFactory.GetDataAccessType(userInput);
+            
+
+
+
+            var products = database.LoadData();
+            database.SaveData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: { product.Name} Price: {product.Price}");
+            }
         }
     }
 }
---
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product(){Name = "PS5", Price = 500},
+            new Product(){Name = "XBox", Price = 350},
+            new Product(){Name = "DrumSet", Price = 1000},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from SQL Data Access.");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a SQL Data Access");
+        }
+    }
+}
